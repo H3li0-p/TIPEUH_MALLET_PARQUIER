8525adfaffe5611143e5572cd0a775e243a1a7cd
H3li0-p <helio.parquier@gmail.com>
2025-04-30 15:35:08 +0200

    modif implem

--- codes/Graph.py
+++ codes/Graph.py
@@ -1,34 +1,14 @@
 """Graphes non-orienté """
 class Graph :
-  
+    
     def __init__(self,nv):     
-         self.nb_edges = 0
-         self.mat = [[-1 for _ in range(nv)] for _ in range(nv)]
-         self.nb_vertices = nv
- 
-    def get_adj_tab(self):
-        """Renvoie une liste d'adjacence associant à chaque sommet ses voisins directs, par ordre croissant de distance les séparants"""
-        nv = self.nb_vertices - 1 #Les nv sommets sont étiquetés de 0 à nv-1
-        mat_adj = self.mat
-        tab_adj = [[] for _ in range(nv)] #tableau contenant nv listes, la i-eme liste contenant les voisins du i-eme sommet
-            for sommet in range(nv):
-                for voisin in range(nv):
-                    if ( mat_adj[sommet][voisin]!= -1):
-                        tab_adj[sommet].append(voisin) # on ajoute le voisin à la liste si il est bien voisin direct du sommet étudié
-        return tab_adj.sorted()
 
-"""Note : la fonction get_adj_tab a un coût élevé (améliorable ?), mais permet par la suite d'accéder aux plus proches voisins de chaque sommet en temps cst
-"""
+        self.nb_edges = 0
+        self.mat = [[-1 for _ in range(nv)] for _ in range(nv)]
+        self.nb_vertices = nv
+        self.mat_lin = [-1 for _ in range((nv*(nv-1))/2)] #version linéarisée de la matrice d'adjacence
+        self.voisins = [[] for _ in range(nv)]
 
-    def get_lin_mat(self):
-        """Donne la version linéarisée de la matrice d'adjacence du graphe : celui-ci étant non-orienté, sa matrice d'adjacence est symétrique"""
-        nv = self.nb_vertices - 1 #-1 pour pouvoir itérer (les nv sommets sont étiquetés de 0 à n-1
-        mat_adj = self.mat
-        lin_adj = []
-        for i in range(nv):
-            for j in range(i+1,nv):
-                lin_adj.append(mat_adj[i][j])
-        return lin_adj
 
                             """[ [-1, 3, 4, 7,-1, 1], exemple de matrice complète, la linéarisation donnerait: 
                                  [ 3,-1, 5, 3, 6,-1],    [3,4,7,-1,1,5,3,6,-1,2,4,1,-1,7,3] (on garde le triangle supérieur qu'on "lit" ligne par ligne)
@@ -38,4 +18,50 @@ class Graph :
                                  [ 1,-1, 1, 7, 3,-1]] 
                                             ^
                                             j               """
+
+    
+    def update_mat_lin(self,u,v,w):
+        """met les champs à jour lors de l'ajout/suppression d'un arc"""
+        nv = self.nb_vertices
+        i = min(u,v)
+        j = max(u,v)
+        self.mat_lin["""calcul d'indice pour mat triangle sup"""] = w 
+       
+    def update_voisins(self,u,v,w):
+        nv = self.nb_vertices
+        voisins_u = self.voisins[u]
+        voisins_u.append(v)
+        nb_nb = len(voisins_u)
+        debut = 0
+        fin = nb_nb
+
+        mat_lin = self.mat_lin
+        i = min(u,v)
+        j = max(u,v)
+
+        """idée : insertion dichotomique dans la liste des voisins (qui est déjà triée)"""
+        #utiliser : voisins_u = voisins_u[:a] + [j] + voisins_u[a:] ?
+
+
+    def add_edge(self,u,v,float weight):
+        """Ajout d'une arrête entre les sommets u et v"""
+        nv = self.nb_vertices
+        assert (weight > 0),"Distance invalide"
+        assert (u!=v),"Pas de boucles (sommets identiques"
+        assert (u<nv and v < nv), "InvalidVertices"
+
+        self.update_mat_lin(u,v,weight)
+        self.update_voisins(u,v,weight)
+
+    def remove_edge(self,u,v):
+
+    def get_voisins(self):
+        return self.voisins
+
+    def get_lin_adj(self):
+        return self.mat_lin
+
+
+
+
 
