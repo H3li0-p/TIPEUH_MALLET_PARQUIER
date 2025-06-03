fa421ba7e2b1bd473eae39a0acd714a29eba8728
H3li0-p <helio.parquier@gmail.com>
2025-06-03 17:57:48 +0200

    modif de graph_carre + ajout de clustering pour les grps

---
+++ codes/clustering.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def creer_tab_voisins(commandes):
+    """Crée un dictionnaire à partir des sommets à visiter, les clés sont les sommets de la liste de commandes, 
+    et les valeurs associée sont une liste des autres sommets à visiter, par ordre croissant de distance au sommet courant"""
+    tabvoisin = {}
+    
+
+
+    return tabvoisin
+
+
+def créer_grp(commandes,tab_v,siz = 3):
+    """crée des groupes de 3 sommets (ou moins si aucun sommet ne reste) qui seront attribués à différents livreurs"""
+    G = []
+    return G
+
